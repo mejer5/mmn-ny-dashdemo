fbe931b6fbb90f99ff390089881960b2608a346e
mejer5 <magnus.mejer@hotmail.com>
2022-03-25 11:26:49 +0100

    done

--- app.py
+++ app.py
@@ -35,6 +35,18 @@ fig_employee = px.bar(order,
 fig_employee.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
 
+
+#****************************************
+# Diagram - Product Sales
+#****************************************
+fig_products = px.bar(order, 
+    x='productname', y='total', 
+    color='type', text='total', title='Product Sales',
+    hover_data=[],
+    labels={'total':'Total sales', 'productname':'Product', 'type':'Product Type'})
+fig_products.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_products.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
 # ***************************************
 # Activate the app
 # ***************************************
@@ -52,7 +64,7 @@ dash_app.layout = html.Div(
                 children=[
                     html.Div(className='four columns div-user-controls',
                             children=[
-                                html.H2('Sales dashboard'),
+                                html.H2('Sales by Product'),
                                 html.P('Select filters from dropdown'),
 
                     html.Div(children="Month", className="menu-title"),
@@ -69,12 +81,37 @@ dash_app.layout = html.Div(
                     ),
                     html.Div(className='eight columns div-for-charts bg-grey',
                             children=[
-                                dcc.Graph(id="sales_employee", figure=fig_employee)
+                                dcc.Graph(id="sales_employee", figure=fig_products),
+                                dcc.Graph(id="emp_name", figure=fig_employee)
+                                       
                             ]
                     ),
+                    html.Div(className='four columns div-user-controls',
+                            children=[
+                                html.H2('Sales by Employee'),
+                                html.P('Select filters from dropdown'),
+                                
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month1',
+                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
+                            ),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year1',
+                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
+                            
+                            
+                    ),
+                    
+                    
+                    
                 ]
             )
         ]
+    
+        )        
+    ]
 )
 
 # ***************************************
@@ -84,7 +121,8 @@ dash_app.layout = html.Div(
 # Input er DropDown
 @dash_app.callback(Output('sales_employee', 'figure'),
               [Input('drop_month', 'value')],
-              [Input('drop_year', 'value')])
+              [Input('drop_year', 'value')],)
+
 
 def update_graph(drop_month, drop_year):
     if drop_year:
@@ -107,9 +145,35 @@ def update_graph(drop_month, drop_year):
         y = order_fig1['total']
             )
         ]
-    }
+    } 
 
 
+@dash_app.callback(Output('emp_name', 'figure'),
+              [Input('drop_month1', 'value')],
+              [Input('drop_year1', 'value')],)
+
+def update_graph1(drop_month1, drop_year1):
+    if drop_year1:
+        if drop_month1:
+            # Data i både drop_month og drop_year
+            order_fig1 = order.loc[(order['orderyear'] == drop_year1) & (order['ordermonth'] == drop_month1)]
+        else:
+            # Data i drop_year. men ikke drop_month
+            order_fig1 = order.loc[order['orderyear'] == drop_year1]
+    else:
+        if drop_month1:
+            # Data i drop_month, men ikke drop_year
+            order_fig1 = order.loc[order['ordermonth'] == drop_month1]
+        else:
+            # Ingen data - ikke noget valgt
+            order_fig1 = order
+        
+    return {'data':[go.Bar(
+        x = order_fig1['emp_name'],
+        y = order_fig1['total']
+            )
+        ]
+    } 
 
 # ***************************************
 # Run the app
